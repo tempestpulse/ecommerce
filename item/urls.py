dab918b98806f726f0a2a36c124a8ada63dd6687
tempestpulse <bulsar@tutanota.com>
2024-02-23 14:58:28 +0100

    add CRUD operations

--- item/urls.py
+++ item/urls.py
@@ -2,11 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from item.views import HomeView, ItemDetailView
+from item.views import HomeView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView
 
 app_name = 'item'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('<int:pk>/', ItemDetailView.as_view(), name='item-detail')
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
+    path('item/<int:pk>/update', ItemUpdateView.as_view(), name='item-update'),
+    path('item/create', ItemCreateView.as_view(), name='item-create'),
+    path('item/<int:pk>/delete', ItemDeleteView.as_view(), name='item-delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-
+from django.http import Http404
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Item
 
 
@@ -14,3 +15,37 @@ class ItemDetailView(DetailView):
     model = Item
     template_name = 'item/item.html'
     context_object_name = 'item'
+
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
+    model = Item
+    fields = ['name', 'price', 'description', 'photo', 'category', 'owner']
+
+    def get_success_url(self):
+        return reverse('item:item-detail', kwargs={'pk': self.object.pk})
+
+
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
+    model = Item
+    fields = ['name', 'price', 'description', 'photo', 'category', 'owner']
+
+    def get_success_url(self):
+        return reverse('item:item-detail', kwargs={'pk': self.object.pk})
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().owner != self.request.user:
+            raise Http404('You are not the owner of this item')
+        return super().dispatch(request, *args, **kwargs)
+
+
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = Item
+    context_object_name = 'item'
+    success_url = reverse_lazy('item:home')
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().owner != self.request.user:
+            raise Http404('You are not the owner of this item')
+        return super().dispatch(request, *args, **kwargs)
+
+
