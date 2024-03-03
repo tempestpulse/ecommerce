2315a873d1616098142476c0c9b6827f9bd1d802
adrian <bulsar@tutanota.com>
2024-03-03 14:37:34 +0100

    add FollowUnfollowView and ProfileDeleteView

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from account.views import RegisterView, CustomLoginView, ProfileDetailView
+from account.views import RegisterView, CustomLoginView, ProfileDetailView, ProfileDeleteView, FollowUnfollowView
 from account import views
 
 app_name = 'account'
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile-detail'),
+    path('profile/<int:pk>/', ProfileDetailView.as_view(), name='profile-detail'),
+    path('profile/<int:pk>/follow', FollowUnfollowView.as_view(), name='follow-unfollow'),
+    path('profile/<int:pk>/delete', ProfileDeleteView.as_view(), name='profile-delete'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import login, logout
 from django.db import models
-from account.forms import CustomUserCreationForm
+from django.http import Http404
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, DeleteView
+from django.views import View
 
-from account.models import Profile
+from account.forms import CustomUserCreationForm
+from account.models import Profile, Follower
 from item.models import Item
 
 
@@ -51,6 +55,42 @@ class ProfileDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         profile = self.object
         item_list = Item.objects.filter(owner=profile.user)
-
         context['item_list'] = item_list
+
+        user = profile.user
+        follower_count = Follower.follower_count(user)
+        following_count = Follower.following_count(user)
+        context['follower_count'] = follower_count
+        context['following_count'] = following_count
+
         return context
+
+
+class ProfileDeleteView(DeleteView):
+    model = Profile
+    context_object_name = 'profile'
+    success_url = reverse_lazy('item:home')
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().user != request.user:
+            raise Http404("You are not the owner of the profile")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class FollowUnfollowView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        follow_status = Follower.objects.filter(user=user, followed_by=request.user).exists()
+
+        if request.user == user:
+            return redirect('account:profile-detail', pk=user.profile.pk)
+
+        if follow_status:
+            Follower.objects.filter(user=user, followed_by=request.user).delete()
+        else:
+            Follower.objects.create(user=user, followed_by=request.user)
+        return redirect('account:profile-detail', pk=user.profile.pk)
+
+
+
+
