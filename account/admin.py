825d669ee13b552d563fd0b9ccd79503aae824f0
adrian <bulsar@tutanota.com>
2024-03-03 14:36:24 +0100

    add Follower model

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from account.models import Profile
+from account.models import Profile, Follower
 
 admin.site.register(Profile)
+admin.site.register(Follower)
--- account/models.py
+++ account/models.py
@@ -7,6 +7,8 @@ class Profile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default=DEFAULT_AVATAR, upload_to='avatars')
+    bio = models.CharField(max_length=50, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.avatar:
@@ -15,3 +17,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    followed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followed_by')
+    date_followed = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def follower_count(user):
+        return Follower.objects.filter(user=user).all().count()
+
+    @staticmethod
+    def following_count(followed_by):
+        return Follower.objects.filter(followed_by=followed_by).all().count()
+
+    def __str__(self):
+        return f'{str(self.user.username)} followed by {str(self.followed_by)}'
