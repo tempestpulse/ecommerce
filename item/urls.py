25707b8b0058e2d6fc6ab9e15ac972adef7a13a6
tempestpulse <bulsar@tutanota.com>
2024-03-05 16:32:58 +0100

    add ItemSearchListView for searching items

--- item/urls.py
+++ item/urls.py
@@ -2,14 +2,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from item.views import HomeView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView
+from item.views import ItemListView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, ItemSearchListView
 
 app_name = 'item'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', ItemListView.as_view(), name='home'),
     path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
     path('item/<int:pk>/update', ItemUpdateView.as_view(), name='item-update'),
     path('item/create', ItemCreateView.as_view(), name='item-create'),
     path('item/<int:pk>/delete', ItemDeleteView.as_view(), name='item-delete'),
+    path('search/', ItemSearchListView.as_view(), name='item-search')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- item/views.py
+++ item/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import Http404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -5,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Item
 
 
-class HomeView(ListView):
+class ItemListView(ListView):
     model = Item
     template_name = 'item/home.html'
     context_object_name = 'item_list'
@@ -19,10 +20,14 @@ class ItemDetailView(DetailView):
 
 class ItemCreateView(LoginRequiredMixin, CreateView):
     model = Item
-    fields = ['name', 'price', 'description', 'photo', 'category', 'owner']
+    fields = ['name', 'price', 'description', 'photo', 'category']
 
     def get_success_url(self):
         return reverse('item:item-detail', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
 
 class ItemUpdateView(LoginRequiredMixin, UpdateView):
@@ -49,3 +54,21 @@ class ItemDeleteView(LoginRequiredMixin, DeleteView):
         return super().dispatch(request, *args, **kwargs)
 
 
+class ItemSearchListView(ListView):
+    model = Item
+    context_object_name = 'items'
+    template_name = 'item/item_search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', None)
+        if query is not None:
+            return Item.objects.filter(
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
+        return Item.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', None)
+        return context
